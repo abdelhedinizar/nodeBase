900f7d41a915683fbac0893e24c92bd20f231edd
abdelehdinizar <abdelhedinizarcss@gmail.com>
2024-09-01 16:38:26 +0200

    html

--- index.js
+++ index.js
@@ -2,17 +2,20 @@ const http = require('http');
 const router = require('router');
 
 const server = http.createServer((req, res) => {
-  res.writeHead(200, {'Content-Type': 'text/html'});
+  res.writeHead(200, {
+    'Content-Type': 'text/html',
+  });
   res.write('<html>');
   res.write('<head>');
   res.write('<title>Node Base</title>');
   res.write('</head>');
   res.write('<body>');
   res.write('<h1> Welcome to Node Base</h1>');
+  res.write('<h2>Mahdi ya bouterma kbira :D</h2>'); 
   res.write('</body>');
   res.end('</html>');
 });
 
-server.listen(8080,( err, res) => {
-    console.log('Server is running on port 8080');
+server.listen(8080, (err, res) => {
+  console.log('Server is running on port 8080');
 });

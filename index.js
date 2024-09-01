11de08225f6b22998dbdca360234d3f11c417a60
abdelehdinizar <abdelhedinizarcss@gmail.com>
2024-09-01 14:17:39 +0200

    index.js

--- index.js
+++ index.js
@@ -1,8 +1,15 @@
 const http = require('http');
+const router = require('router');
 
 const server = http.createServer((req, res) => {
   res.writeHead(200, {'Content-Type': 'text/plain'});
-  res.end('Welcome to My Node Base Application');
+  res.end('<html>');
+  res.end('<head>');
+  res.end('<title>Node Base</title>');
+  res.end('</head>');
+  req.end('<body>');
+  res.end('<h1> Welcome to Node Base</h1>');
+  res.end('</body>');
 });
 
 server.listen(8080,( err, res) => {

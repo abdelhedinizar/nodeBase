a47b6a7968ad158440abcd0c5fe9619c051c7979
abdelehdinizar <abdelhedinizarcss@gmail.com>
2024-09-02 19:51:42 +0200

    index

--- index.js
+++ index.js
@@ -1,22 +1,13 @@
-const http = require('http');
+const express = require('express');
+
+const app = express();
+const port = 8080;
+
+app.get('/', (req, res) => {
+  res.status(200).send('Welcome to Node Base');
+});
+
+app.listen(port, () => {
+  console.log(`Running on port ${port}...`);
+});
 const router = require('router');
-
-const server = http.createServer((req, res) => {
-  res.writeHead(200, {
-    'Content-Type': 'text/html',
-  });
-  res.write('<html>');
-  res.write('<head>');
-  res.write('<title>Node Base</title>');
-  res.write('</head>');
-  res.write('<body>');
-  res.write('<h1> Welcome to Node Base</h1>');
-  res.write('<h2>Development in progress !</h2>'); 
-  res.write('<h2>Base ! !</h2>'); 
-  res.write('</body>');
-  res.end('</html>');
-});
-
-server.listen(8080, (err, res) => {
-  console.log('Server is running on port 8080');
-});

ebc9134093d931f3c320d7b257e74e3c1cc90aa9
abdelehdinizar <abdelhedinizarcss@gmail.com>
2024-11-04 15:45:35 +0100

    SAFOR-23 : Paiement web service

--- routes/bookingRoutes.js
+++ routes/bookingRoutes.js
@@ -4,5 +4,9 @@ const bookingController = require('../controllers/bookingController');
 const route = express.Router();
 
 route.get('/checkout-seesion/:orderID', bookingController.getCheckoutSeesion);
-route.post('/webhook', bookingController.webhookCheckout);
+route.post(
+  '/webhook',
+  express.raw({ type: 'application/json' }),
+  bookingController.webhookCheckout
+);
 module.exports = route;

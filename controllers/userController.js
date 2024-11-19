023b473918b193585913796967dd3d9b5486796b
abdelehdinizar <abdelhedinizarcss@gmail.com>
2024-11-19 19:44:44 +0100

    SAFOR-18 : User modelling + rest web service

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,7 +1,26 @@
 const User = require('./../models/UserModel');
+const APIFeatures = require('./../utils/APIFeatures');
 
-const getAllUsers = (req, res) => {
-  res.status(200).send('Hello Elliot et Khaled :D !');
+const getAllUsers = async (req, res) => {
+  try {
+    const features = new APIFeatures(User.find(), req.query)
+      .filter()
+      .sort()
+      .limitFields()
+      .paginate();
+
+    const users = await features.query;
+    res.status(200).json({
+      status: 'success',
+      length: users.length,
+      data: { users },
+    });
+  } catch (e) {
+    res.status(400).json({
+      status: 'fail',
+      message: e,
+    });
+  }
 };
 
 const getUserById = (req, res) => {
@@ -22,14 +41,20 @@ const deleteUser = (req, res) => {
 };
 
 const getUserByEmail = (req, res) => {
-  if(!req.email){
+  if (!req.email) {
     res.status(400).send('Please provide an email');
   }
   const user = User.findOne({ email: req.email });
-  if(!user){
+  if (!user) {
     res.status(404).send('User not found');
   }
   res.status(200).send(user);
 };
 
-module.exports =  { getAllUsers, getUserById, createUser, updateUser, deleteUser };
+module.exports = {
+  getAllUsers,
+  getUserById,
+  createUser,
+  updateUser,
+  deleteUser,
+};

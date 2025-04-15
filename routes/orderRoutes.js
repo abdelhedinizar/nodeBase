28229d68872cd3c388d5caf7be53b590692d55d8
abdelehdinizar <abdelhedinizarcss@gmail.com>
2025-04-15 23:12:09 +0200

    orders statistic

--- routes/orderRoutes.js
+++ routes/orderRoutes.js
@@ -4,13 +4,18 @@ const {
   createOrder,
   getOrderById,
   updateOrder,
+  getOrderStats,
   getOrderBySessionId,
 } = require('../controllers/OrderController');
 const authController = require('../controllers/AuthController');
 
 const route = express.Router();
 
+route.route('/stats').get(getOrderStats);
 route.route('/').get(getAllOrders).post(createOrder);
-route.route('/:id').get(getOrderById).patch(authController.protect,updateOrder);
+route
+  .route('/:id')
+  .get(getOrderById)
+  .patch(authController.protect, updateOrder);
 
 module.exports = route;
